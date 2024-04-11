78935d1c49a3485ec2a5d4181100545dfd7c95ff
Bilal Hussain <bilalhussain42201@gmail.com>
2024-04-11 22:27:03 +0500

    update

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,26 @@
+// let score = "333abc"; //NaN
+// let score = "333"; //number
+// let score = null; //0
+// console.log(score);
+// console.log(typeof score);
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log("types: ",typeof valueInNumber)
+// console.log('valueInNumber: ', valueInNumber);
+
+// let isloggedIn = 1
+// let isloggedIn = ""
+// let isloggedIn = "Bilal";
+// let BooleanIsLoggedIn = Boolean(isloggedIn);
+// console.log("BooleanIsLoggedIn: ", BooleanIsLoggedIn);
+
+//1 => true ; 0 => false
+// "" => false
+//"bilal" => true
+
+
+let someNumber = 333
+let stringNum = String(someNumber)
+console.log(stringNum)
+console.log(typeof stringNum);
---
+++ 01_basic/04_comperation.js
@@ -0,0 +1,54 @@
+// let numb = 3
+// let negNum = -numb;
+// console.log(negNum)
+
+// console.log(2 + 2)
+// console.log(2 - 2)
+// console.log(2 / 2)
+// console.log(2 % 2)
+// console.log(2 ** 2)
+// console.log(2 * 2)
+
+// let str1 = "hello "
+// let str2 = "Bilal"
+// console.log(str1 + str2)
+
+// console.log(1 + "2")
+// console.log("1" + 2)
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+// console.log((3 + 4) + 4 % 4)
+
+// console.log(true)
+// console.log(+true)
+// console.log(+"")
+
+// let num1, num2, num3;
+// num1 = num2 = 2 + 3
+// let gameCounter = 100;
+// ++gameCounter;
+// console.log(gameCounter);
+
+
+// console.log(2 >= 1)
+// console.log(2 > 1)
+// console.log(2 <= 1)
+// console.log(2 < 1)
+// console.log(2 != 1)
+// console.log(2 == 1)
+
+
+console.log(null == 0)
+console.log(null >= 0)
+console.log(null <= 0)
+console.log(null != 0)
+console.log("------------------")
+console.log(undefined == 0)
+console.log(undefined >= 0)
+console.log(undefined <= 0)
+console.log(undefined != 0)
+
+
+console.log("\n===")
+console.log("2" === 2)

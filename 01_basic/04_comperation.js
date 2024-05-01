048747da598015208c2ecef01bea956954379284
Bilal Hussain <bilalhussain42201@gmail.com>
2024-05-01 21:04:44 +0500

    update

--- 01_basic/04_comperation.js
+++ 01_basic/04_comperation.js
@@ -39,16 +39,16 @@
 // console.log(2 == 1)
 
 
-console.log(null == 0)
-console.log(null >= 0)
-console.log(null <= 0)
-console.log(null != 0)
-console.log("------------------")
-console.log(undefined == 0)
-console.log(undefined >= 0)
-console.log(undefined <= 0)
-console.log(undefined != 0)
+// console.log(null == 0)
+// console.log(null >= 0)
+// console.log(null <= 0)
+// console.log(null != 0)
+// console.log("------------------")
+// console.log(undefined == 0)
+// console.log(undefined >= 0)
+// console.log(undefined <= 0)
+// console.log(undefined != 0)
 
 
-console.log("\n===")
-console.log("2" === 2)
+// console.log("\n===")
+// console.log("2" === 2)

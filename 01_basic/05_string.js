6856ddafc3cf39c3deb14a1cd8dacc01bfa7c4c3
Bilal Hussain <bilalhussain42201@gmail.com>
2024-05-04 10:33:34 +0500

    string

---
+++ 01_basic/05_string.js
@@ -0,0 +1,14 @@
+const name = "Bilal"
+const repoCount = 34
+console.log(name + repoCount)
+
+console.log(`my name is ${name} and ${repoCount}`)
+
+const gameName = new String("akmal Jutt")
+console.log(gameName)
+console.log(gameName[0])
+console.log(gameName.__proto___)
+console.log(gameName.toUpperCase())
+console.log(gameName.length)
+console.log(gameName.charAt(0)) //indexing number 0 = 'a'
+console.log(gameName.indexOf('a')) // character 'a' = 0 

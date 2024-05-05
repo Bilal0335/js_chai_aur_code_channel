1392aef6c20dd8820cd859eb32d54d2229d1ebf4
Bilal0335 <bilalhussain42201@gmail.com>
2024-05-05 23:57:39 +0500

    Math continue

--- 01_basic/06_math_and_math.js
+++ 01_basic/06_math_and_math.js
@@ -14,8 +14,40 @@
 // const balanceString = balance.toString();
 // console.log("string Number: ", balanceString);
 // console.log("length: ", balanceString.length);
+// console.log(Number.MAX_VALUE)
+// console.log(Number.MIN_VALUE)
+// console.log(Number.MAX_SAFE_INTEGER)
+// console.log(Number.isFinite())
+
+// const balance = new Number(30).valueOf()
+// const balances = balance.toString()
+// console.log(balances.length)
 
 
-const balance = new Number(30).valueOf()
-const balances = balance.toString()
-console.log(balances.length)
+// let a = 455.7575
+// console.log(a.toFixed(2))
+
+// let b = 12545.67
+// console.log(b.toPrecision(3))
+
+// let c = 55335553
+// console.log(c.toLocaleString())
+// console.log(c.toLocaleString('en-PK'))
+// console.log(c.toLocaleString('en-IN'))
+// console.log(c.toLocaleString('ar-EG'))
+
+//! Math
+
+console.log(Math)
+// console.log(Math.abs(-44)) //absolute - converter +
+// console.log(Math.round(4.5))
+// console.log(Math.ceil(4.2)) //top or highesr
+// console.log(Math.floor(4.2)) //down or lower 
+// console.log(Math.sqrt(4.2))
+// console.log(Math.pow(5,2)) 
+// console.log(Math.tan(5)) 
+// console.log(Math.max(5,88,6,7,7,8)) 
+// console.log(Math.min(5,88,6,7,7,8)) 
+
+
+console.log(Math.random() * 2 + 1)

169eb3a1e8f6b276c24f9e50cb703a94bf24f08d
Bilal0335 <bilalhussain42201@gmail.com>
2024-05-05 14:58:36 +0500

    Number and Math

---
+++ 01_basic/06_math_and_math.js
@@ -0,0 +1,21 @@
+// const score = 4000
+// console.log('score: ', score);
+
+// const balance = new Number(40)
+// console.log('balance: ', balance);
+
+// console.log("string Number: ",balance.toString())
+// console.log("string Number: ",balance.length)
+
+
+// const balance = new Number(40).valueOf();
+// console.log('balance: ', balance);
+
+// const balanceString = balance.toString();
+// console.log("string Number: ", balanceString);
+// console.log("length: ", balanceString.length);
+
+
+const balance = new Number(30).valueOf()
+const balances = balance.toString()
+console.log(balances.length)

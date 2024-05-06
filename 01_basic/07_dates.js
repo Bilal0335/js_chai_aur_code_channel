e325b68f6e0851d18c775401cec49b613f7fd2ee
Bilal0335 <bilalhussain42201@gmail.com>
2024-05-06 23:28:02 +0500

    date and time

---
+++ 01_basic/07_dates.js
@@ -0,0 +1,46 @@
+//* Date
+let date = new Date();
+// console.log(date);
+// console.log(date.toString());
+// console.log(date.toISOString())
+// console.log(date.toLocaleString())
+// console.log(date.toLocaleTimeString())
+// console.log(date.toDateString())
+// console.log(date.toTimeString())
+// console.log(date.toJSON())
+// console.log(date.getFullYear())
+// console.log(date.getMonth())
+// console.log(date.getDay())
+// console.log(date.getTime())
+
+// console.log(typeof date);
+
+// let createDate = new Date(2024, 4, 6);
+// console.log(createDate.toDateString());
+// let createDate = new Date(2024, 4, 6,3);
+// console.log(createDate.toLocaleString());
+
+// let createDate = new Date("2024-04-01")
+// console.log(createDate.toLocaleString())
+// let createDate = new Date("01-04-2023")
+// console.log(createDate.toLocaleString())
+
+
+//* Time compare 
+let myCreateDate = new Date();
+let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+// console.log(myCreateDate.getTime());
+
+// console.log(Math.floor(Date.now()/1000)) //* milliscond convert second
+
+
+let newDate = new Date()
+console.log(newDate);
+// console.log(newDate.getTime());
+// console.log(newDate.getDate());
+// console.log(newDate.getDay());
+// console.log(newDate.getFullYear());
+console.log(newDate.getMonth() + 1); //01 mean add 4 increase 5
+console.log(newDate.getMonth()); // 0 strat index
+// console.log(newDate.valueOf());
